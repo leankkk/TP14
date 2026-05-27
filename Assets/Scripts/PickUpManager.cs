57960774a2378a5d71d9ce8fda1d8d0084294604
leankkk <leankonito@gmail.com>
2026-05-27 14:30:37 -0300

    Clase 2: Mecánica de recolección funcional y modelo con materiales

---
+++ Assets/Scripts/PickUpManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class PickUpManager : MonoBehaviour
+{
+    int contador = 0;
+    public TextMeshProUGUI textoContador;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Coleccionable"))
+        {
+            contador++;
+            textoContador.text = "Lápices: " + contador;
+            Debug.Log("Lápices recolectados: " + contador);
+            Destroy(other.gameObject);
+        }
+    }
+}
